150a5d357c6941c26f3512705d4a09a27fc55daa
Lucas Nolting <lnolting22@gmail.com>
2019-02-24 16:47:59 -0600

    inital commit

---
+++ WorkingCalc/WorkingCalc/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WorkingCalc
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+       
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            double calculatedTip = 0;
+
+            //var percentEntry = new Entry { Text = "Percent" };
+            var percentVal = Convert.ToDouble(percent.Text);
+
+            //var costEntry = new Entry { Text = "Meal Cost" };
+            var costVal = Convert.ToDouble(cost.Text);
+
+            calculatedTip = percentVal * costVal;
+           
+            ((Button)sender).Text = $"Your Tip Is {calculatedTip}";
+        }
+    }
+}
